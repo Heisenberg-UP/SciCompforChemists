40e7d798ed1642d22d37b5371b5777d9fa5a5d8a
Heisenberg Uncertainty Principle <99283516+Heisenberg-UP@users.noreply.github.com>
2023-01-24 17:57:17 -0600

    Update CH3_Exercises.py

--- notebooks/chapter_03/CH3_Exercises.py
+++ notebooks/chapter_03/CH3_Exercises.py
@@ -24,6 +24,7 @@ plt.scatter(Volume, ideal_gas(Volume), c='green') # Sets up data
 plt.xlabel('Volume (L)') # X label
 plt.ylabel('Pressure (atm)') # Y Label
 plt.title('Pressure as a function of Volume') # Title of graph
+
 plt.show()
 
 
@@ -38,7 +39,8 @@ plt.scatter(AN, Xp, c=a_radii) # Scatter plot for AN and Xp with c being colorba
 plt.colorbar() # Implements color bar
 plt.xlabel('Atomic Number') # X label
 plt.ylabel('Electronegativity') # Y label
-plt.title('Electronegativity of halogens')
+plt.title('Electronegativity of halogens') # Graph Title
+
 plt.show()
 
 
@@ -75,6 +77,7 @@ plt.plot(x, f(x)[2], color='blue', label='h(x)') # Plots h(x) function
 plt.xlabel('X values') # X labels
 plt.ylabel('Y values') # Y labels
 plt.title('Sandwich Thereom Example') # Title for function
+
 plt.show()
 
 
@@ -105,6 +108,7 @@ plt.plot(t, first_order_kinetics(t), color='green') # Plots data provided
 plt.xlabel('Time (s)') # X label
 plt.ylabel('Concentration [A]') # Y label
 plt.title('First Order Kinetics\n2A --> P') # Graph title
+
 plt.show()
 
 
@@ -119,6 +123,7 @@ plt.plot(df['time'], df['abs_intensity'], color='red') # Sets up graph
 plt.xlabel('Time (min)') # X label
 plt.ylabel('Intensity') # Y label
 plt.title('Gas Chromatography Graph') # Graph title
+
 plt.show()
 
 
@@ -133,6 +138,7 @@ plt.stem(df['m/z'], df['abs_intensity'], markerfmt=' ') # Plots data
 plt.xlabel('m/z') # X label
 plt.ylabel('Intensity') # Y label
 plt.title('Mass Spectrum Bromobenzene') # Graph title
+
 plt.show()
 
 
@@ -144,6 +150,7 @@ percents = [78, 21, 1] # Percents of gases in atmosphere
 plt.pie(percents, labels=['N$_2$', 'O$_2$', 'Other Gases'], explode=(0, 0, 0.2)) # Plots data
 plt.title('Atmospheric Composition') # Graph title
 plt.axis('equal') # I dont know what this does
+
 plt.show()
 
 
@@ -156,6 +163,7 @@ rdn = [random.random() for value in range(1000)] # Creates random list
 plt.hist(rdn, bins=10, edgecolor='k') # Plots data into 10 bins
 plt.xlabel('Numerical Value') # X label
 plt.ylabel('Number of Numerical Values') # Y label
+
 plt.show()
 
 
@@ -169,7 +177,8 @@ plt.stem(ppm, intensity, markerfmt=' ') # Plots data
 plt.xlabel('ppm') # x label
 plt.ylabel('Intensity') # Y label
 plt.title('$^1$H NMR Spectrum of Caffeine in CDCl$_3$') # Graph title
-plt.gca().invert_xaxis()
+plt.gca().invert_xaxis() # Makes the graph look like a regular NMR graph
+
 plt.show()
 
 
@@ -182,9 +191,75 @@ kcal = [0.0, 11.6, 9.8, 13.4, 5.8, 8.3, 2.7] # y axis
 plt.step(step, kcal) # Plots graph
 plt.xlabel('Step number') # X label
 plt.ylabel('Energy (Kcal)') # Y label
-plt.title('Energy per step in the binding and splittle of H$_2$(g)')
+plt.title('Energy per step in the binding and splittle of H$_2$(g)') # Graph title
+
 plt.show()
 
 
 # %% Exercise 11
 import matplotlib.pyplot as plt # Imports plotting library
+
+AN = list(range(1, 11, 1)) # generates list from 0 to 10
+Radii = [53, 31, 167, 112, 87, 67, 56, 48, 42, 38] # Atomic radii in picometers
+IE1 = [13.60, 24.59, 5.39, 9.32, 8.30, 11.26, 14.53, 13.62, 17.42, 21.56] # IE1 in eV
+
+plt.figure(figsize=(10, 6)) # Creates a figure for multiple plots
+
+plt.subplot(1, 2, 1) # First subplot
+plt.plot(AN, Radii, color='green') # Plots data
+plt.xlabel('Atomic Number') # X label
+plt.ylabel('Radii (pm)') # Y label
+plt.title('Radii of Elements') # Graph title
+
+plt.subplot(1, 2, 2) # Second Plot
+plt.plot(AN, IE1, color='green') # Plots data
+plt.xlabel('Atomic Number') # X label
+plt.ylabel('First Ionization Energy (eV)') # Y label
+plt.title('First Ionization Energies of Elements') # Graph title
+
+plt.show()
+
+
+# %% Exercise 12
+import matplotlib.pyplot as plt # Imports plotting library
+import numpy as np # Imports mathematical/array library
+
+x = np.linspace(0, 1, 20) # Creates array of values for x
+y = np.linspace(0, 1, 20) # Creates array of values for y
+X, Y = np.meshgrid(x, y) # Creates a surface for which to operate mathemetical functions on
+
+
+def Particle_Box(x, y, nx=2, ny=2, L=1):
+    '''
+    This Function will calculate a 2D surface of for the "Particle in a box" analogy.
+    Enter the values of x and y. 
+    The function assumes n = 2 and L = 1
+    '''
+    psi = (2 / L) * np.sin((nx * np.pi * x) / L) * np.sin((ny * np.pi * y) / L)
+    return psi
+
+
+fig = plt.figure(figsize=(10, 6)) # Sets figure size
+ax = fig.add_subplot(111, projection='3d') # Makes plot 3D
+ax.plot_surface(X, Y, Particle_Box(X, Y), cmap='viridis') # Plots Data
+
+plt.show()
+
+
+# %% Exercise 13
+import matplotlib.pyplot as plt # Imports plotting library
+import pandas as pd # Imports data manipulation library
+
+Data = pd.read_csv('data/amine_bp.csv')
+df = pd.DataFrame(Data, columns=['carbons', 'primary', 'secondary', 
+                                 'tertiary'])
+
+plt.scatter(df['primary'], df['carbons'], marker='o', color='red', label='Primary') # Plots primary degree amine data
+plt.scatter(df['secondary'], df['carbons'], marker='s', color='blue', label='Secondary') # Plots secondary degree amine data
+plt.scatter(df['tertiary'], df['carbons'], marker='.', color='green', label='Tertiary') # Plots tertiary degree amine data
+plt.legend(loc='best') # Assigns a legend and location
+plt.xlabel('Temperature (C)') # X label
+plt.ylabel('Number of carbons') # Y label
+plt.title('Boiling Point of Amines') # Title of graph
+
+plt.show()
